95a2185304a6860639d25b6c50e889c4b8cde982
Itsmewho <jordy_mt@live.nl>
2024-11-11 23:23:44 +0100

    The next day

---
+++ CS50p/importing.py
@@ -0,0 +1,9 @@
+def hello(name):
+    print(f"Hello, {name}")
+
+
+def goodbye(name):
+    print(f"Goodbye, {name}")
+
+
+# C say.py for the use of funtions.
--- CS50p/randomchoice.py
+++ CS50p/randomchoice.py
@@ -1,12 +1,46 @@
 import random
+import sys
+import statistics
 
 # coin = random.choice(["Heads", "Tails"])
 
 # player = input("What is your choice? \n")
 # print(coin)
 
+# cards = ["Jack", "Queen", "King", "Ace"]
+# random.shuffle(cards)
+# for card in cards:
+#     print(card)
 
-cards = ["Jack", "Queen", "King", "Ace"]
-random.shuffle(cards)
-for card in cards:
-    print(card)
+# print(statistics.mean([100, 90]))  # for average
+
+# print("hello, my name is", sys.argv[1])
+# # for these things the documentation is importent to read.
+
+# try:
+#     print("Hello, my name is", sys.argv[1])
+# except IndexError:
+#     print("No argument found in terminal.")
+
+# if len(sys.argv) < 2:
+#     print("Too few arguments")
+# elif len(sys.argv) > 2:
+#     print(" To many arguments")
+# else:
+#     print("Hello, my name is", sys.argv[1])
+
+# Exit statements.
+
+# if len(sys.argv) < 2:
+#     sys.exit("Too few arguments")
+# elif len(sys.argv) > 2:
+#     sys.exit("Too many arguments")
+
+# print("Hello my name is", sys.argv[1])
+
+
+# if len(sys.argv) < 2:
+#     sys.exit("To fer args")
+
+# for arg in sys.argv[1:-1]:  # slices
+#     print("Hello, my name is", arg)
