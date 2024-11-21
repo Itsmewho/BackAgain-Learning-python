d6d48bcd541509f05468801191929251d656ecf1
Itsmewho <jordy_mt@live.nl>
2024-11-21 20:52:15 +0100

    hangman starter file

---
+++ 100Days/hangman.py
@@ -0,0 +1,11 @@
+# Make a hangman game.
+
+# Use a dict -> import the dict from another file.
+# Give each word a hint as helpline.
+# Give a second helpline. Show 2 letters, if not the last 2 letters of the word.
+# Add difficulty levels.
+# High score -> CRUD operations -> Remember player.
+# Refactor the game-over condition so the game stops immediately when a player loses all lives or chooses to quit. (quit any time)
+
+# Use the style of 'boiler plate'.
+# Try to think about the comments, redundent code and readability.
