ab4a084dbbb49e2fe43f318bc852f321e5b45a76
Itsmewho <jordy_mt@live.nl>
2024-11-21 20:54:59 +0100

    hangman starter file

--- 100Days/hangman.py
+++ 100Days/hangman.py
@@ -5,7 +5,11 @@
 # Give a second helpline. Show 2 letters, if not the last 2 letters of the word.
 # Add difficulty levels.
 # High score -> CRUD operations -> Remember player.
+# Show last highscore and player
+
 # Refactor the game-over condition so the game stops immediately when a player loses all lives or chooses to quit. (quit any time)
+
+# Mabe add some ASCII art. Keep the ASCII and remove the input to keep terminal clean???????
 
 # Use the style of 'boiler plate'.
 # Try to think about the comments, redundent code and readability.
